019301e04059311bd24ac4cb553e9c056265f55f
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2025-02-08 12:20:04 +0530

    testing change

--- src/components/posthog-provider.tsx
+++ src/components/posthog-provider.tsx
@@ -10,7 +10,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) return;
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
         api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        capture_pageview: false // Disable automatic pageview capture, as we capture manually
+        capture_pageview: true // Disable automatic pageview capture, as we capture manually
       })
   }, [])
 
