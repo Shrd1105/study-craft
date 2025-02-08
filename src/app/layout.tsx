53d82298beaaff6479b3bd44c90b72bc2a26aeeb
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2025-02-08 11:46:38 +0530

    added new analytics tool

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import { Inter, Lexend } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Header } from "@/components/Header";
-import { Analytics } from "@vercel/analytics/react"
 import { NextAuthProvider } from "@/providers/NextAuthProvider";
+import { PostHogProvider } from '@/components/posthog-provider';
+import dynamic from "next/dynamic";
+
+const PostHogPageView = dynamic(() => import('@/components/PostHogPageView'), { ssr: false });
 
 const inter = Inter({
   subsets: ['latin'],
@@ -34,10 +37,12 @@ export default function RootLayout({
         className={`${inter.variable} ${lexend.variable} font-sans antialiased min-h-screen bg--background`}
       >
         <NextAuthProvider>
+          <PostHogProvider>
           <Header />
+          <PostHogPageView />
           {children}
           <Toaster />
-          <Analytics />
+          </PostHogProvider>
         </NextAuthProvider>
       </body>
     </html>
